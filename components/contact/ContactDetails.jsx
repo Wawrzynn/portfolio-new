219456d6937b2912ba27722b160dadeecf303a4c
Wawrzyn <jedrzejkosakowski@gmail.com>
2023-11-03 14:46:47 +0100

    Added contact form.

---
+++ components/contact/ContactDetails.jsx
@@ -0,0 +1,79 @@
+import { FiPhone, FiMapPin, FiMail } from "react-icons/fi";
+
+// const contacts = [
+//   {
+//     id: 1,
+//     name: "Poznań, Poland",
+//     icon: <FiMapPin />,
+//   },
+//   {
+//     id: 2,
+//     name: "email@domain.com",
+//     icon: <FiMail />,
+//   },
+//   {
+//     id: 3,
+//     name: "555 8888 888",
+//     icon: <FiPhone />,
+//   },
+// ];
+
+function ContactDetails() {
+  const poznanCoordinates = "52.407795,16.933761";
+  
+  return (
+    <div className="w-full lg:w-1/2">
+      <div className="text-left max-w-xl px-6">
+        <h2 className="font-general-medium text-2xl text-primary-dark dark:text-primary-light mt-12 mb-8">
+          Contact details
+        </h2>
+        {/* <ul>
+          {contacts.map((contact) => (
+            <li className="flex " key={contact.id}>
+              <i className="text-2xl text-neutral-500 dark:text-neutral-400 mr-4 mt-1">
+                {contact.icon}
+              </i>
+              <span className="text-lg mb-4 text-ternary-dark dark:text-ternary-light">
+                {contact.name}
+              </span>
+            </li>
+          ))}
+        </ul> */}
+        <ul>
+          <li>
+            <a href="tel:+48509949726" className="flex">
+              <i className="text-2xl text-neutral-500 dark:text-neutral-400 mr-4 mt-1">
+                <FiPhone />
+              </i>
+              <span className="text-lg mb-4 text-ternary-dark dark:text-ternary-light">
+                +48 509 949 726
+              </span>
+            </a>
+          </li>
+          <li>
+          <a href="mailto:jedrzejkosakowski@gmail.com" className="flex">
+              <i className="text-2xl text-neutral-500 dark:text-neutral-400 mr-4 mt-1">
+                <FiMail />
+              </i>
+              <span className="text-lg mb-4 text-ternary-dark dark:text-ternary-light">
+                jedrzejkosakowski@gmail.com
+              </span>
+            </a>
+          </li>
+          <li>
+          <a href={`geo:${poznanCoordinates}`} target="_blank" rel="noopener noreferrer" className="flex">
+              <i className="text-2xl text-neutral-500 dark:text-neutral-400 mr-4 mt-1">
+                <FiMapPin />
+              </i>
+              <span className="text-lg mb-4 text-ternary-dark dark:text-ternary-light">
+               Poznań, Poland
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default ContactDetails;
--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,27 @@
-export default function Projects() {
-  return <h1>Contact Page</h1>
+import { motion } from "framer-motion";
+import ContactDetails from "../components/contact/ContactDetails";
+import ContactForm from "../components/contact/ContactForm";
+import MetaHeaders from "../components/global/MetaHeaders";
+
+export default function contact() {
+  return (
+    <div>
+      <MetaHeaders title="Contact" />
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          ease: "easeInOut",
+          duration: 0.5,
+          delay: 0.1,
+        }}
+        className="container mx-auto flex flex-col-reverse lg:flex-row py-5 lg:py-10 lg:mt-5"
+      >
+        <ContactForm />
+
+        <ContactDetails />
+      </motion.div>
+    </div>
+  );
 }
