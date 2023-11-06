393deccb140e17e8213a9d9e6bde0fd4cb26467c
Wawrzyn <jedrzejkosakowski@gmail.com>
2023-11-06 18:23:03 +0100

    Another fix

--- components/home/Header.jsx
+++ components/home/Header.jsx
@@ -2,23 +2,23 @@ import Image from "next/image";
 import CVButton from "../reusable/CVButton";
 
 export default function Header() {
-  const text = "Hi, I' m";
+  const text = "I\'m a full-stack web"
 
   return (
     <section>
       <div className="flex sm:flex-row w-full justify-around items-center mt-14 flex-col">
         <div className="flex flex-col sm:items-start items-center">
           <h1 className="text-4xl font-medium text-primary-dark dark:text-primary-light">
-           {text} <span className="text-secondary">Jędrzej</span>
+            Hello, my name is <span className="text-secondary">Jędrzej</span>
           </h1>
           <br />
           <p className="text-lg text-primary-dark dark:text-primary-light">
-            I'm a full-stack web developer
+           {text}
           </p>
           <p className="text-lg text-stone-500 dark:text-primary-light">
             and a student of Computer Science
           </p>
-            <CVButton />
+          <CVButton />
         </div>
 
         <div>
