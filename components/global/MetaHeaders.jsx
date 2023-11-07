8c552226e09a821a776f869ebf677001f1032967
Wawrzyn <jedrzejkosakowski@gmail.com>
2023-11-07 17:59:59 +0100

    Changed meta tags

--- components/global/MetaHeaders.jsx
+++ components/global/MetaHeaders.jsx
@@ -7,6 +7,14 @@ function PagesMetaHead({ title, keywords, description }) {
       <meta name="keywords" content={keywords} />
       <meta name="description" content={description} />
       <meta charSet="utf-8" />
+      <meta name="author" content="Jędrzej Kosakowski" />
+      <meta name="robots" content="index, follow" />
+      <meta name="lang" content="en" />
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content="website" />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content="Portfolio project" />
+      <meta property="og:locale" content="en" />
       <link rel="icon" href="/favicon.ico" />
       <title>{title}</title>
     </Head>
