e18f592321cf80b4858e059305e435e87e602761
Wawrzyn <jedrzejkosakowski@gmail.com>
2023-11-07 14:52:29 +0100

    Utils deleted.

--- utils/utils.js
+++
@@ -1,7 +0,0 @@
-export const kebabCase = (str) =>
-  str
-    .replace(/([a-z])([A-Z])/g, "$1-$2")
-    .replace(/[\s_]+/g, "-")
-    .toLowerCase();
-
-export const kebabArray = (arr) => arr.map((item) => kebabCase(item));
